e46234e930dd2d14e2965f74c7246d51cc7479f1
SlimyZockt <slimyzockt77@gmail.com>
2025-08-11 21:17:38 +0200

    save file

--- addons/ressourcedb/ressourcedb.gd
+++ addons/ressourcedb/ressourcedb.gd
@@ -1,28 +1,35 @@
 @tool
 extends EditorPlugin
 
-var db_path := "res://data"
+var db_path := "res://addons/ressourcedb/demo/"
 var filesystem: EditorFileSystem
 var db := {}
 var db_script := "" 
-var file_popup := EditorFileDialog.new() 
+var file_popup := EditorFileDialog.new()
 
 func select_path(path: String):
 	db_path = path
+	var path_save_file := FileAccess.open("res://addons/ressourcedb/path.txt", FileAccess.WRITE)
+	path_save_file.store_string(db_path)
+	path_save_file.close()
+	filesystem.scan()
 
 func _enter_tree():
 	file_popup.file_mode = EditorFileDialog.FILE_MODE_OPEN_DIR
-	file_popup.filters = PackedStringArray([""])
 	file_popup.dir_selected.connect(select_path)
 	filesystem = EditorInterface.get_resource_filesystem()
 	filesystem.filesystem_changed.connect(_on_filesystem_changed)
+	var path_save_file := FileAccess.open("res://addons/ressourcedb/path.txt", FileAccess.READ)
+	db_path = path_save_file.get_as_text()
+	path_save_file.close()
+	
 	EditorInterface.get_editor_main_screen().add_child(file_popup)
 	add_autoload_singleton("DB", "res://addons/ressourcedb/database.gd")
 	add_tool_menu_item("Set DB Path", file_popup.popup_file_dialog)
 
 func _on_filesystem_changed():
-	if not DirAccess.dir_exists_absolute(db_path): return
 	var dir = DirAccess.open(db_path)
+	if dir == null: return
 	db = {}
 	gen_db(dir)
 	db_script = "extends Node \n\n"	
@@ -34,17 +41,19 @@ func _on_filesystem_changed():
 		db_script += "}\n"
 	
 	if FileAccess.file_exists("res://addons/ressourcedb/database.gd"):
-		var file = FileAccess.open("res://addons/ressourcedb/database.gd", FileAccess.READ)
+		var file := FileAccess.open("res://addons/ressourcedb/database.gd", FileAccess.READ)
 		if file.get_as_text() == db_script: return 
 	var file = FileAccess.open("res://addons/ressourcedb/database.gd", FileAccess.WRITE)
 	file.store_string(db_script)
+	file.close()
+	filesystem.scan()
 
 func gen_db(source: DirAccess):
 	var dir = DirAccess.open(source.get_current_dir())
 	var dir_name = source.get_current_dir().get_file()
 	db[dir_name] = {}
 	for file in dir.get_files():
-		if not file.ends_with(".tres") && not file.ends_with(".res") && not file.ends_with(".tscn"): return
+		if not file.ends_with(".tres") && not file.ends_with(".res") && not file.ends_with(".tscn"): continue
 		var temp = file
 		if file.ends_with(".tres"): temp = temp.substr(0, temp.length() - 5)
 		if file.ends_with(".res"): temp = temp.substr(0, temp.length() - 4)
